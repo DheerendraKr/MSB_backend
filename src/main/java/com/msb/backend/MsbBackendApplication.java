48d8144c9da90029fc0866a1ee2d8735f109a87b
Dheerendra Kumar <dheeexitrendra173@outlook.com>
2020-08-30 15:41:30 +0530

    initial packages added

---
+++ src/main/java/com/msb/backend/MsbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.msb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbBackendApplication.class, args);
+	}
+
+}
